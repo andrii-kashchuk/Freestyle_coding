cefdc68719f09e557bb8f60e0739fe109d4e9e11
Andrey <fullstack2021@gmail.com>
2021-06-24 16:40:06 +0300

    Bulb switcher with transformation of the images

---
+++ Bulb_switcher/js/script.js
@@ -0,0 +1,8 @@
+function turnOn() {
+    let image = document.getElementById('picture').src='./assets/bulb-on.png';
+}
+
+function turnOff() {
+    let image = document.getElementById('picture').src='./assets/bulb-off.png';
+    return image;
+}
