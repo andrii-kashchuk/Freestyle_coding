e4ad179d038fd6ea1d142c9f9dda166a9cae0dbb
Andrey <fullstack2021@gmail.com>
2021-11-03 13:48:53 +0200

    Responsive Web Design course is finished

---
+++ FreeCodeCamp/Personal Portfolio Webpage/js/script.js
@@ -0,0 +1,19 @@
+/*
+
+let textBefore = "<";
+let textAfter = "/>";
+
+let text = document.getElementById('text-tribute');
+text.textContent = "Tribute Page";
+
+text.addEventListener("mouseout", function() {
+    text.textContent = "Tribute Page";
+    text.style.color = "white";
+})
+
+text.addEventListener("mouseover", function() {
+    text.style.color = "red";
+    text.textContent = `${textBefore} Tribute Page ${textAfter}`;
+})
+
+*/
